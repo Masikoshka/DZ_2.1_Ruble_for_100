619b439463d3f03f378b6d132e00ee5d64f8e2df
masikoshka <sheve-masha@yandex.ru>
2021-05-13 23:49:45 +0300

    -v1

---
+++ src/Main.java
@@ -0,0 +1,17 @@
+public class Main {
+    public static void main(String[] args) {
+        int balance = 258;
+        int refill = 1015;
+        int bonusRubles;
+        int totalBalance;
+
+        if (refill >= 1000) {
+            bonusRubles = refill / 100;
+        } else {
+            bonusRubles = 0;
+        }
+        totalBalance = balance + refill + bonusRubles;
+        System.out.println("Итоговая сумма на счету клиента: " + totalBalance + " рублей");
+        System.out.println("Бонус составил: " + bonusRubles + " рублей");
+    }
+}
